404db4ac1d282e37ff250a75793d8571f8b469cb
AaronDex <100770396+AaronDex@users.noreply.github.com>
2022-05-10 20:57:04 +0930

    new

---
+++ twoup/coinToss.java
@@ -0,0 +1,21 @@
+public class coinToss{
+    static Boolean coin1=coin.genCoin1();
+    static Boolean coin2=coin.genCoin2();
+
+public static String coinToss1() {
+    if(coin1 == true){
+        String coin1Toss=("Heads");
+        return coin1Toss;   
+    }else{
+        String coin1Toss1=("Tails");
+        return coin1Toss1;}
+}
+public static String coinToss2() {
+    if(coin2 == true){
+        String coin1Toss=("Heads");
+        return coin1Toss;   
+    }else{
+        String coin1Toss1=("Tails");
+        return coin1Toss1;
+    }
+}}
---
+++ twoup/twoUp.java
@@ -0,0 +1,8 @@
+public class twoUp {
+    public static void main(String[] args) {                   
+        String coin1= coinToss.coinToss1();
+        String coin2= coinToss.coinToss2();
+        System.out.println(coin1);
+        System.out.println(coin2);
+    }
+    }
