73bfc890b305afd600078452040bd2459d453153
AaronDex <aaron_herasingh@outlook.com>
2022-05-16 17:49:09 +0930

    Proper Username and pass checking

--- python fishing/fishingGame.py
+++ python fishing/fishingGame.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2.token import NEWLINE
 from random import randint
 class fishableItem:
     def __init__(self,name,keeper,fish,pointsKept,pointsReleased):
@@ -19,8 +20,25 @@ class fishTable:
     largeMullet=fishableItem("Large Mullet",True,True,20,20)
     seaweed=fishableItem("Seaweed",True,False,5,-5)
 def userInfoGet():
-    userDetails=user(input("Please Enter a Username "),input(("Please Enter a Password ")),0)
-    
+    accountStatus=input("Do You Have a Account? Y/N ")
+    if accountStatus=="N":
+        userDetails=user(input("Please Enter a Username "),input(("Please Enter a Password ")),0)
+        file = open("demofile.txt","a")
+        file.write(userDetails.userName+" "+userDetails.password+" "+0+"\n")
+        file.close
+    else:
+        while True:
+            userDetails=user(input("Please Enter Your Username "),input(("Please Enter Your Password ")),0)
+            try:
+                for line in open("demofile.txt","r").readlines():
+                    loginDetails=line.split()
+                    if userDetails.userName==loginDetails[0] and userDetails.password==loginDetails[1]:
+                        print("welcome back")
+                        update=user(loginDetails[0],loginDetails[1],loginDetails[2])
+                        return False
+            except:
+                print("Wrong Username Or Password")
+
 def goFishing():
     cast=randint(1,6)
     
