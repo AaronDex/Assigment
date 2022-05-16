9c34db4aaa87fa146392e6b1a270ef7c52dfc457
AaronDex <aaron_herasingh@outlook.com>
2022-05-16 15:20:48 +0930

    new

--- python fishing/fishingGame.py
+++ python fishing/fishingGame.py
@@ -1,1 +1,10 @@
-szcxvfvsadhjkbfdkhjasfdhguika
+class fishableItem:
+    def __init__(self,keeper,fish,pointsKept,pointsReleased):
+        self.keeper = keeper
+        self.fish = fish
+        self.pointsKept= pointsKept
+        self.pointsReleased =pointsReleased
+kingGW=fishableItem(True,True,50,70)
+bait=fishableItem(False,False,-10,0)
+undersizedFish=fishableItem(False,True,-10,0)
+print("kingGW")
